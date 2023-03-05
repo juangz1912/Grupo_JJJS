ee88b6c0c150662f86c6e7ec50fafce26d5b6fad
14jmgc03 <115053318+14jmgc03@users.noreply.github.com>
2023-03-05 17:42:22 -0500

    Add files via upload
    Se hicieron ajustes al menú, corrección de errores.

--- Taller_2_JJJS/Menu(prototipo).py
+++ Taller_2_JJJS/Menu(prototipo).py
@@ -1,59 +1,59 @@
 import Formulas
 
 def obtener_formula():
-    orden = input("¿Influye el orden? (s/n): ").upper
-    elementos = input("¿Entran todos los elementos? (s/n): ").upper
-    repiten = input("¿Los elementos se repiten? (s/n): ").upper
+    orden = input("¿Influye el orden? (s/n): ").lower()
+    elementos = input("¿Entran todos los elementos? (s/n): ").lower()
+    repiten = input("¿Los elementos se repiten? (s/n): ").lower()
 
 
     if orden == "s" and elementos == "n" and repiten =="n":
-        m = int(input("Ingrese m: "))
-        n = int(input("Ingrese n: "))
+        m = int(input("Ingrese la población total: "))
+        n = int(input("Ingrese la muestra(cantidad tomada): "))
         formula = "m!/(m-n)!"
         resultado = Formulas.variacion_ordinaria(m, n)
 
-        return (formula, resultado)
+        return formula, resultado
 
     elif orden == "s" and elementos == "n" and repiten =="s":
-        m = int(input("Ingrese m: "))
-        n = int(input("Ingrese n: "))
+        m = int(input("Ingrese la población total: "))
+        n = int(input("Ingrese la muestra(cantidad tomada): "))
         formula = "m^n"
         resultado = Formulas.variacion_repetida(m, n)
 
-        return (formula, resultado)
+        return formula, resultado
 
     elif orden == "s" and elementos == "s" and repiten =="n":
-        m = int(input("Ingrese m: "))
+        m = int(input("Ingrese la población total: "))
         formula = "m!"
         resultado = Formulas.permutacion_ordinaria(m)
 
-        return (formula, resultado)
+        return formula, resultado
 
     elif orden == "s" and elementos == "s" and repiten =="s":
-        m = int(input("Ingrese m: "))
-        a = int(input("Ingrese n: "))
-        b = int(input("Ingrese n: "))
-        c = int(input("Ingrese n: "))
+        m = int(input("Ingrese la población total: "))
+        a = int(input("Ingrese la muestra(cantidad 1): "))
+        b = int(input("Ingrese la muestra(cantidad 2): "))
+        c = int(input("Ingrese la muestra(cantidad 3): "))
         formula = "m!/a!*b!*c!"
         resultado = Formulas.permutacion_repetida(m, a, b, c)
 
-        return (formula, resultado)
+        return formula, resultado
     
     elif orden == "n" and elementos == "n" and repiten =="n":
-        m = int(input("Ingrese m: "))
-        n = int(input("Ingrese n: "))
+        m = int(input("Ingrese la población total: "))
+        n = int(input("Ingrese la muestra(cantidad tomada): "))
         formula = "m!/n!*(n-m)!"
         resultado = Formulas.combinacion_ordinaria(m, n)
 
-        return (formula, resultado)
+        return formula, resultado
 
     elif orden == "n" and elementos == "n" and repiten =="s":
-        m = int(input("Ingrese m: "))
-        n = int(input("Ingrese n: "))
+        m = int(input("Ingrese la población total: "))
+        n = int(input("Ingrese la muestra(cantidad tomada): "))
         formula = "(m+n-1)!/n!*(m-1)!"
         resultado = Formulas.combinacion_repetida(m, n)
 
-        return (formula, resultado)
+        return formula, resultado
     
 # Ejemplo de uso
 formula, resultado = obtener_formula()
