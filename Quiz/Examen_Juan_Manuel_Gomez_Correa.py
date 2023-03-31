7e8cefea3115fb2bcc1b8bfcedc119e2f1fb219b
14jmgc03 <115053318+14jmgc03@users.noreply.github.com>
2023-03-31 14:59:28 -0500

    Examen #1
    Examen #1

---
+++ Quiz/Examen_Juan_Manuel_Gomez_Correa.py
@@ -0,0 +1,24 @@
+def menuInicial():
+    numeroInt = int(input("Digite el número: "))
+    print(cantidadDigitos(numeroInt))
+    print("NO RECURSIVO: ", cantidadDigitosNoR(numeroInt))
+
+def cantidadDigitos(numeroInt):
+    if numeroInt == 0:
+        return 0
+    numeroInt = numeroInt//10
+    return cantidadDigitos(numeroInt) + 1
+
+def cantidadDigitosNoR(numeroInt):          #O(n)
+    contador = 0                        #O(1)
+    while numeroInt != 0:               #O(n)
+        numeroInt = numeroInt//10       #O(n)
+        contador += 1                   #O(n)
+
+    # O(1) + O(n) + O(n) + O(n)
+    # = 3n+1 = O(n)
+    
+    return contador                     #O
+
+menuInicial()
+print("Respuesta No Recuersiva: ", cantidadDigitosNoR(123456789)) #O(n)
