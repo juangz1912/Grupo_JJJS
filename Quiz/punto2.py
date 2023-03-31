671125adef42ec1c95fb48198430d5765f6f2253
juangz1912 <93444656+juangz1912@users.noreply.github.com>
2023-03-31 15:04:46 -0500

    Juan_Jose_Giraldo

---
+++ Quiz/punto2.py
@@ -0,0 +1,32 @@
+def potencia(f, n):
+    F = 1 #O(1)
+    for i in range(n):#O(n)
+        F = F * f #O(1)
+    return F #O(n)
+
+#Big O =2(1) + 2(n)
+
+print(potencia(5,5))
+
+def potencia_recursivo(f,n,R=1):
+    if n == 0:
+        return R
+    else:
+        R = R*f
+        N = n-1
+        return potencia_recursivo(f, N, R)
+
+print(potencia_recursivo(5,5))
+
+def factorial(numero):
+  if numero==0 :
+    return 1
+  else:
+      times = numero*(factorial(numero-1))
+      return times
+
+def permutacion_ordinaria(numero):
+    formula = factorial(numero)
+    return formula
+
+print(permutacion_ordinaria(5))
