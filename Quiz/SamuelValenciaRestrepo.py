18ef4d98b4e19cddddef6035544940110d6331d4
Samael <91275451+Igniz2001@users.noreply.github.com>
2023-03-31 15:13:39 -0500

    Quiz Samuel

---
+++ Quiz/SamuelValenciaRestrepo.py
@@ -0,0 +1,9 @@
+def f(n):#----O(n)
+    if n < 0:#-----O(1)
+        return#-----O(1)
+    else:#-----O(1)
+        string = str(n)#-----O(1)
+        lenght = len(string)#-----O(1)
+        return lenght#-----O(1)
+    
+#6(1)+1(n)
