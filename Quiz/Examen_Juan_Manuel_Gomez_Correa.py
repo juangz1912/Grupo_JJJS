a3dafecb017bfa1ddd97c63a1bdd55194ff95ea8
juangz1912 <93444656+juangz1912@users.noreply.github.com>
2023-03-31 15:18:37 -0500

    Update Examen_Juan_Manuel_Gomez_Correa.py

--- Quiz/Examen_Juan_Manuel_Gomez_Correa.py
+++ Quiz/Examen_Juan_Manuel_Gomez_Correa.py
@@ -18,7 +18,7 @@ def cantidadDigitosNoR(numeroInt):          #O(n)
     # O(1) + O(n) + O(n) + O(n)
     # = 3n+1 = O(n)
     
-    return contador                     #O
+    return contador                     #O(n)
 
 menuInicial()
 print("Respuesta No Recuersiva: ", cantidadDigitosNoR(123456789)) #O(n)
