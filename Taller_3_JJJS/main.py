86cacdfafe1b50f669053667e951562c606e812d
JuanK <juancamilo.manjarres@gmail.com>
2023-04-23 16:55:37 -0500

    API añadida

--- Taller_3_JJJS/main.py
+++ Taller_3_JJJS/main.py
@@ -1,5 +1,15 @@
+import requests
+import json
+from pandas import json_normalize
 import Sorts
-lista = [7, 69, 34, 0, 5, 105]
+link = "https://www.datos.gov.co/resource/gt2j-8ykr.json"
+data = requests.get(link)
+datos = json.loads(data.text)
+datos = json_normalize(datos)
+print(datos.edad.values)
+
+lista = datos.edad.values
+print(lista)
 print("Ingrese el tipo de sort que quiere realizar ")
 print("1. Heap Sort")
 print("2. Merge Sort")
@@ -7,19 +17,17 @@ print("3. Quick Sort")
 print("4. Counting Sort")
 print("5. Radix Sort")
 print("6. Bubble Sort")
-print(lista)
 sort_type = input()
 
-match sort_type:
-    case 1:
-        Sorts.Sort.heap_sort(lista)
-    case 2:
-        Sorts.Sort.MergeSort(lista)
-    case 3:
-        Sorts.Sort.quick_sort(lista)
-    case 4:
-        Sorts.Sort.counting_sort(lista)
-    case 5:
-        Sorts.Sort.radix_sort(lista)
-    case 6:
-        Sorts.Sort.bubble_sort(lista)
+if sort_type == 1:
+    Sorts.Sort.heap_sort(lista)
+elif sort_type == 2:
+    Sorts.Sort.MergeSort(lista)
+elif sort_type == 3:
+    Sorts.Sort.quick_sort(lista)
+elif sort_type == 4:
+    Sorts.Sort.counting_sort(lista)
+elif sort_type == 5:
+    Sorts.Sort.radix_sort(lista)
+elif sort_type == 6:
+    Sorts.Sort.bubble_sort(lista)
