8c259ac5ce93420164089f98285d75d18f9b5fea
IGNIZ <samael041203@gmail.com>
2023-04-23 19:34:39 -0500

    Modificaciones en el main
    al sort_type le puse que fuera int(input()) ya que tomaba el dato como un string, y cambié el import Sorts por from Sorts import Sort para instanciar la clase en una variable llamada sorter y así no me mostrar un error de argumento

--- Taller_3_JJJS/main.py
+++ Taller_3_JJJS/main.py
@@ -1,7 +1,9 @@
 import requests
 import json
 from pandas import json_normalize
-import Sorts
+from Sorts import Sort
+
+sorter = Sort()
 link = "https://www.datos.gov.co/resource/gt2j-8ykr.json"
 data = requests.get(link)
 datos = json.loads(data.text)
@@ -17,17 +19,17 @@ print("3. Quick Sort")
 print("4. Counting Sort")
 print("5. Radix Sort")
 print("6. Bubble Sort")
-sort_type = input()
+sort_type = int(input())
 
 if sort_type == 1:
-    Sorts.Sort.heap_sort(lista)
+    sorter.heap_sort(lista)
 elif sort_type == 2:
-    Sorts.Sort.MergeSort(lista)
+    sorter.MergeSort(lista)
 elif sort_type == 3:
-    Sorts.Sort.quick_sort(lista)
+    sorter.quick_sort(lista)
 elif sort_type == 4:
-    Sorts.Sort.counting_sort(lista)
+    sorter.counting_sort(lista)
 elif sort_type == 5:
-    Sorts.Sort.radix_sort(lista)
+    sorter.radix_sort(lista)
 elif sort_type == 6:
-    Sorts.Sort.bubble_sort(lista)
+    sorter.bubble_sort(lista)
